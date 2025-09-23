5edf43343ac0a65eb23bd17f24bb372cd8747b61
islam sayed <islam.sayed291@gmail.com>
2025-09-23 10:18:39 +0300

    feØat: add CustomText widget for reusable text styling

---
+++ lib/widgets/custom_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText({super.key,
+    required this.text,
+    required this.color,
+     this.fontSize=14,
+     this.fontWeight=FontWeight.normal,});
+
+final String text;
+final Color color;
+final double fontSize;
+final FontWeight fontWeight;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    );
+  }
+}

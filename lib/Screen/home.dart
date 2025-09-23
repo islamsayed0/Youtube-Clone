47c20cd40b6295f3968f7b1807c398bfe3dc92f7
islam sayed <islam.sayed291@gmail.com>
2025-09-23 11:14:33 +0300

    feat : make appbar

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import '../widgets/app_bar.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  TextEditingController searchController = TextEditingController();
+  FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title:App_Bar(
+            controller: searchController,
+            logo: 'assets/img/logo_app.png',
+            prefixIcon: Icons.cancel,
+            suffixIcon: Icons.search,
+            onsubmit: (v){},
+            ontap: (){},
+          )
+        ),
+      ),
     );
   }
 }

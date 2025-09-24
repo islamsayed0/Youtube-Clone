9ad35e81643c5d8a86f3633f712799040a88eb21
islam sayed <islam.sayed291@gmail.com>
2025-09-24 15:40:28 +0300

    feat✨ : adding_const_file

---
+++ lib/const/string.dart
@@ -0,0 +1,7 @@
+class string {
+  static final base_url = 'https://youtube-media-downloader.p.rapidapi.com';
+  static final Map <String , String> headers = {
+    'x-rapidapi-key': '8ea0407e23msh7a2701c99ca11a0p126878jsnb6c20d41b43d',
+    'x-rapidapi-host': 'youtube-media-downloader.p.rapidapi.com'
+  };
+}
